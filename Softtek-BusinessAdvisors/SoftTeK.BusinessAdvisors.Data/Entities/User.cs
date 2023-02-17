96c83a7c4b93644a159679fd0ac5c611faf0f411
Max Aguilar <zz30.maxzz@gmail.com>
2023-02-17 00:45:14 -0500

    Adding entities

---
+++ Softtek-BusinessAdvisors/SoftTeK.BusinessAdvisors.Data/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SoftTeK.BusinessAdvisors.Data.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+
+        [JsonIgnore]
+        public string? PasswordHash { get; set; }
+    }
+}
---
+++ Softtek-BusinessAdvisors/SoftTeK.BusinessAdvisors.Data/Interface/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using SoftTeK.BusinessAdvisors.Data.Entities;
+
+namespace SoftTeK.BusinessAdvisors.Data.Interface
+{
+    public interface IUserService
+    {
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+        void Create(User user);
+        void Update(int id, User user);
+        void Delete(int id);
+    }
+}
